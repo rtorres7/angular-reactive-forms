9e379f3e12362e053b0813a0f464828ffdcc1217
rtorres7 <shortysupreme@gmail.com>
2020-03-18 19:27:32 -0400

    angular reactive form 21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+// import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-forms';
+
+  get userName() {
+    return this.registrationForm.get('userName');
+  }
+  constructor(private fb: FormBuilder) {}
+
+  registrationForm = this.fb.group({
+    userName: ['', [Validators.required, Validators.minLength(3)]],
+    password: [''],
+    confirmPassword: [''],
+    address: this.fb.group({
+      city: [''],
+      state: [''],
+      postalCode: ['']
+    }),
+  });
+
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('t-bone'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl('')
+  //   })
+  // });
+
+  loadApiData() {
+    this.registrationForm.patchValue({
+      userName: 'Xavier',
+      password: 'pa$$word',
+      confirmPassword: 'pa$$word'
+      // address: {
+      //   city: 'Colorado Springs',
+      //   state: 'Colorado',
+      //   postalCode: '90210'
+      // }
+    });
+  }
 }
