044055bee17967d18f24b1ae6cd460a918f29575
rtorres7 <shortysupreme@gmail.com>
2020-03-18 14:23:15 -0400

    angular reactive forms 20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'reactiveForms';
+  registrationForm = new FormGroup({
+    userName: new FormControl("t-bone"),
+    password: new FormControl(""),
+    confirmPassword: new FormControl(""),
+    address: new FormGroup({
+      city: new FormControl(""),
+      state: new FormControl(""),
+      postalCode: new FormControl("")
+    })
+  });
+
+  loadApiData() {
+    this.registrationForm.setValue({
+      userName: "t-bone",
+      password: "password",
+      confirmPassword: "confirmPassword",
+      address: {
+        city: "city",
+        state: "state",
+        postalCode: "12345"
+      }
+    });
+  }
 }
